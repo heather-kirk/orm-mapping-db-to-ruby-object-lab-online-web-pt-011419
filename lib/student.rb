8a678445b510503d5601f95394ea5b28c51d0523
Heather Kirk <hskirk@gmail.com>
2019-04-02 01:18:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,10 +89,11 @@ class Student
     DB[:conn].execute(sql, x)
   end 
   
-  def self.first_student_in_grade_10(first)
+  def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
+    LIMIT 1 
     SQL
     
     DB[:conn].execute(sql).map do |row|
